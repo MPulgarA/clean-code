957807727d61cb4ce5313cc7fba2d0185d69ee70
MpulgarA <matiaspulgaralarcon@gmail.com>
2022-06-15 21:55:17 -0400

    objetos como propiedades

--- src/clean-code/06-clases-a.ts
+++ src/clean-code/06-clases-a.ts
@@ -17,13 +17,46 @@
 
     // Si se quiere extender de la clase padre, se debe crear una instancia de la misma
     class User extends Person {
+        public lastAccess: Date;
+
         constructor(
             public email: string,
             public role: string,
-            private lastAccess: Date,
-        ){
-            super();
+            name: string,
+            gender: Gender,
+            birthdate: Date
+        ) {
+            super(name, gender, birthdate);
+            this.lastAccess = new Date();
+        }
+
+        checkCredentials() {
+            return true;
         }
     }
+
+    // Mantener la sesion de usuario
+    // Estas clases violentan el princio de responsabilidad unica, debido a que hacen demasiadas cosas
+    class UserSettings extends User {
+        constructor(
+            public workingDirectory: string,
+            public lastOpenFolder: string,
+            email: string,
+            role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date
+        ) {
+            super(email, role, name, gender, birthdate);
+        }
+    }
+
+    // const newUser = new User();
+
+    const userSettings = new UserSettings(
+        '/usr/home','/home', 'matpulgar@gmail.com', 'Admin', 'Matias', 'M', new Date('1998-07-17')
+    );
+
+    console.log({userSettings, areCredentialsValid: userSettings.checkCredentials()})
 
 })();
