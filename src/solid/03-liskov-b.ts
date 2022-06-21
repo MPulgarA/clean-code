1c3fcca2c40ed17ba5d67fce39a217a2369f6eb8
MpulgarA <matiaspulgaralarcon@gmail.com>
2022-06-21 00:48:07 -0400

    open-close refactor with abstract classes

--- src/solid/03-liskov-b.ts
+++ src/solid/03-liskov-b.ts
@@ -1,37 +1,74 @@
-export class Tesla {
+// Sirven principalmente para trabajar con herencia
+// Necesario para que todas las subclases de vehiculo tengan los métodos del mismo
+// No se usan para crear instancias, si no para crear herencias
+export abstract class Vehicle {
+    // getNumberOfSits(): number{
+    //     throw Error('Method not implemented.');
+    // }
 
-    constructor(private numberOfSeats: number) { }
+    // Las clases que extiendan de la clase abstracta, deberan implementar los metodos y propiedades abstractas
+    // Si la palabra abstacta no exista, se debe obligar a tener ese metodo
+    abstract getNumberOfSeats(): number;
+}
 
-    getNumberOfTeslaSeats() {
+
+export class Tesla extends Vehicle {
+
+    constructor(private numberOfSeats: number) {
+        super();
+    }
+
+    getNumberOfSeats() {
         return this.numberOfSeats;
+
     }
 }
 
-export class Audi {
+export class Audi extends Vehicle {
 
-    constructor(private numberOfSeats: number) { }
+    constructor(private numberOfSeats: number) {
+        super();
+    }
 
-    getNumberOfAudiSeats() {
+    getNumberOfSeats() {
         return this.numberOfSeats;
+
     }
 }
 
-export class Toyota {
+export class Toyota extends Vehicle { 
 
-    constructor(private numberOfSeats: number) { }
+    constructor(private numberOfSeats: number) {
+        super();
+    }
 
-    getNumberOfToyotaSeats() {
+    getNumberOfSeats() {
         return this.numberOfSeats;
+
     }
 }
 
-export class Honda {
+export class Honda extends Vehicle {
 
-    constructor(private numberOfSeats: number) { }
+    constructor(private numberOfSeats: number) {
+        super();
+    }
 
-    getNumberOfHondaSeats() {
+    getNumberOfSeats() {
         return this.numberOfSeats;
+
     }
 }
 
+export class Kia extends Vehicle {
+
+    constructor(private numberOfSeats: number) {
+        super();
+    }
+
+    getNumberOfSeats() {
+        return this.numberOfSeats;
+
+    }
+}
 
