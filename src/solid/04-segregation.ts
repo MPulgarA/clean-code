04ca4a9026c2e98c5b14c15c1e4ca821470f415c
MpulgarA <matiaspulgaralarcon@gmail.com>
2022-06-21 01:08:48 -0400

    interface segretation refactor

--- src/solid/04-segregation.ts
+++ src/solid/04-segregation.ts
@@ -1,45 +1,44 @@
 (() => {
 
     interface Bird {
-        fly(): void;
         eat(): void;
-        run(): void;
-        swim(): void;
     }
 
     // Debemos segregar entre diferentes interfaces y clases, para no implementar métodos o funciones que no correspondan
-
-    class Tucan implements Bird {
-        public fly() { }
-        public eat() { }
-        public run() { }
-        public swim(){ }
+    interface FlyingBird {
+        fly(): number;
     }
 
-    class Humminbird implements Bird {
-        public fly() { }
-        public eat() { }
-        public run() { }
-        public swim(){ }
+    interface RunningBird {
+        run(): void;
     }
 
-    class Ostrich implements Bird {
-        public fly() {
-            throw new Error('Esta ave no vuela');
-        }
-        public eat() { }
-        public run() { }
-        public swim(){ }
+    interface SwimmerBird {
+        swim(): void;
     }
 
-    
-    class Pinguin implements Bird {
-        public fly() {
-            throw new Error('Esta ave no vuela');
-        }
+
+    class Tucan implements Bird, FlyingBird {
+        public fly() { return 30 }
+        public eat() { }
+
+    }
+
+    class Humminbird implements Bird, FlyingBird {
+        public fly() { return 100 }
+        public eat() { }
+ 
+    }
+
+    class Ostrich implements Bird, RunningBird {
         public eat() { }
         public run() { }
-        public swim(){ }
+    }
+
+
+    class Pinguin implements Bird, SwimmerBird {
+        public eat() { }
+        public swim() { }
     }
 
 })()
